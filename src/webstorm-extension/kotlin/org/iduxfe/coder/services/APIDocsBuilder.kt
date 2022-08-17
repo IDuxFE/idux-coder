627c10f159ad9915a3c0e3fc7204963a70d66ca3
tuchg <ituchg@qq.com>
2022-08-17 15:41:24 +0800

    fix: custom apis invalid

--- src/webstorm-extension/kotlin/org/iduxfe/coder/services/APIDocsBuilder.kt
+++ src/webstorm-extension/kotlin/org/iduxfe/coder/services/APIDocsBuilder.kt
@@ -1,5 +1,6 @@
 package org.iduxfe.coder.services
 
+import com.intellij.javascript.nodejs.library.NodeModulesDirectoryManager
 import org.iduxfe.coder.ui.AppSettingsState
 import org.iduxfe.coder.utils.markdownToHTML
 import kotlinx.serialization.json.Json
@@ -10,62 +11,84 @@ import kotlin.collections.HashMap
 
 val apis: MutableMap<String, String> = HashMap()
 
-val apisFiles = arrayListOf<String>()
+private val iduxDirs = arrayOf(
+  "@idux/cdk",
+  "@idux/components",
+  "@idux/pro",
+  "@idux-vue2/cdk",
+  "@idux-vue2/components",
+  "@idux-vue2/pro"
+).map { "$it/api.json" }
+
+private val apisFiles = arrayListOf<String>()
+private val settingState = AppSettingsState.instance
+
+private fun scanJSONs() {
+  currProject?.let { projectModule ->
+    val nodeModulesInstance = NodeModulesDirectoryManager.getInstance(projectModule)
+    nodeModulesInstance.nodeModulesDirectories.forEach { module ->
+      iduxDirs.forEach { iduxLib ->
+        val r = module.nodeModulesDir.findFileByRelativePath(iduxLib)
+        if (r != null) {
+          apisFiles.add(r.path)
+        }
+      }
+    }
+
+    settingState.customAPIsPath?.let {
+      apisFiles.add(it)
+    }
+  }
+}
 
 fun initAPIMap() {
-    if (apisFiles.isEmpty()) {
-        return
-    }
+  scanJSONs()
 
-//  val fileLoc = "D:\\api.json"
-    apisFiles.forEach {
-        val text = File(it).readText(Charsets.UTF_8)
-        flatAPIs(Json.parseToJsonElement(text) as JsonObject)
-    }
-    mergeCustomAPIs()
+  if (apisFiles.isEmpty()) {
+    return
+  }
+
+  apisFiles.forEach {
+    val text = File(it).readText(Charsets.UTF_8)
+    flatAPIs(Json.parseToJsonElement(text) as JsonObject)
+  }
 }
 
-private fun mergeCustomAPIs() {
-    val settingState = AppSettingsState.instance
-    if (settingState.customAPIsPath != null) {
-        flatAPIs(Json.parseToJsonElement(File(settingState.customAPIsPath!!).readText(Charsets.UTF_8)) as JsonObject)
-    }
-}
 
 private fun flatAPIs(json: JsonObject) {
-    val settingState = AppSettingsState.instance
-    try {
-        json.entries.forEach { comp ->
-            (comp.value as JsonObject).entries.forEach { lang ->
-                (lang.value as JsonObject).entries.forEach { type ->
-                    when (type.key) {
-                        "raw" -> {
-                            apis["${comp.key}.${lang.key}.${type.key}"] =
-                                markdownToHTML((type.value as JsonPrimitive).content, settingState.baseURL)
-                        }
-
-                        "path" -> {
-                        }
-
-                        else -> {
-                            (type.value as JsonObject).entries.forEach {
-                                val compWithLang = lang.value as JsonObject
-                                val path =
-                                    if (compWithLang["path"] == null) "" else "/${(compWithLang["path"] as JsonPrimitive).content}"
-
-                                apis["${comp.key}.${lang.key}.${type.key}.${it.key}"] = markdownToHTML(
-                                    dataMarkdown(it.value as JsonObject),
-                                    "${settingState.baseURL}${path}"
-                                )
-                            }
-                        }
-                    }
-                }
+  val settingState = AppSettingsState.instance
+  try {
+    json.entries.forEach { comp ->
+      (comp.value as JsonObject).entries.forEach { lang ->
+        (lang.value as JsonObject).entries.forEach { type ->
+          when (type.key) {
+            "raw" -> {
+              apis["${comp.key}.${lang.key}.${type.key}"] =
+                markdownToHTML((type.value as JsonPrimitive).content, settingState.baseURL)
             }
+
+            "path" -> {
+            }
+
+            else -> {
+              (type.value as JsonObject).entries.forEach {
+                val compWithLang = lang.value as JsonObject
+                val path =
+                  if (compWithLang["path"] == null) "" else "/${(compWithLang["path"] as JsonPrimitive).content}"
+
+                apis["${comp.key}.${lang.key}.${type.key}.${it.key}"] = markdownToHTML(
+                  dataMarkdown(it.value as JsonObject),
+                  "${settingState.baseURL}${path}"
+                )
+              }
+            }
+          }
         }
-    } catch (e: Exception) {
-        println(e)
+      }
     }
+  } catch (e: Exception) {
+    println(e)
+  }
 }
 
 
@@ -73,7 +96,7 @@ private fun dataWrapper(data: String, tag: String): String = if (data != "-") "*
 
 private fun dataMarkdown(data: JsonObject): String {
 
-    return """
+  return """
 ${(data["description"] as JsonPrimitive).content}
 ---
 ${dataWrapper((data["type"] as JsonPrimitive).content, "type")}
