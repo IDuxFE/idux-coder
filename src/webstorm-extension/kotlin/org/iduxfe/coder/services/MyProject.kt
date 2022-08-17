3cfed8974eeeb85f0b134ccb29ba62e46efd3150
tuchg <ituchg@qq.com>
2022-08-17 15:41:24 +0800

    fix: custom apis invalid

--- src/webstorm-extension/kotlin/org/iduxfe/coder/services/MyProject.kt
+++ src/webstorm-extension/kotlin/org/iduxfe/coder/services/MyProject.kt
@@ -3,12 +3,21 @@ package org.iduxfe.coder.services
 import com.intellij.openapi.components.service
 import com.intellij.openapi.project.Project
 import com.intellij.openapi.project.ProjectManagerListener
-import org.iduxfe.coder.services.MyProjectService
-import com.intellij.javascript.nodejs.library.NodeModulesDirectoryManager
+
+var currProject: Project? = null
 
 internal class MyProjectManagerListener : ProjectManagerListener {
 
-    override fun projectOpened(project: Project) {
-        project.service<MyProjectService>()
-    }
+  override fun projectOpened(project: Project) {
+    project.service<MyProjectService>()
+  }
 }
+
+class MyProjectService(project: Project) {
+
+  init {
+    currProject = project
+    initAPIMap()
+  }
+}
+
